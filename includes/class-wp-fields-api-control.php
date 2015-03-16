8f13e3b23dd5106a417b142c3d50dc1ea27984b8
Scott Kingsley Clark <lol@scottkclark.com>
2015-03-16 06:47:01 -0500

    Control tweaks from customizer

--- includes/class-wp-fields-api-control.php
+++ includes/class-wp-fields-api-control.php
@@ -12,16 +12,14 @@ class WP_Fields_API_Control {
 	 *
 	 * Used when sorting two instances whose priorities are equal.
 	 *
-	 * @since 4.1.0
 	 * @access protected
 	 * @var int
 	 */
-	protected static $instance_count = 0;
+	public static $instance_count = 0;
 
 	/**
 	 * Order in which this instance was created in relation to other instances.
 	 *
-	 * @since 4.1.0
 	 * @access public
 	 * @var int
 	 */
@@ -29,15 +27,15 @@ class WP_Fields_API_Control {
 
 	/**
 	 * @access public
-	 * @var WP_Customize_Manager
+	 * @var string
 	 */
-	public $manager;
+	public $id;
 
 	/**
 	 * @access public
 	 * @var string
 	 */
-	public $id;
+	public $object;
 
 	/**
 	 * All settings tied to the control.
@@ -107,46 +105,44 @@ class WP_Fields_API_Control {
 	public $type = 'text';
 
 	/**
-	 * Callback.
-	 *
-	 * @since 4.0.0
-	 * @access public
-	 *
-	 * @see WP_Customize_Control::active()
-	 *
-	 * @var callable Callback is called with one argument, the instance of
-	 *               WP_Customize_Control, and returns bool to indicate whether
-	 *               the control is active (such as it relates to the URL
-	 *               currently being previewed).
-	 */
-	public $active_callback = '';
-
-	/**
 	 * Constructor.
 	 *
-	 * Supplied $args override class property defaults.
+	 * Parameters are not set to maintain PHP overloading compatibility (strict standards)
 	 *
-	 * If $args['settings'] is not defined, use the $id as the setting ID.
-	 *
-	 * @since 3.4.0
-	 *
-	 * @param WP_Customize_Manager $manager
-	 * @param string $id
-	 * @param array $args
+	 * @return WP_Fields_API_Control $setting
 	 */
-	public function __construct( $manager, $id, $args = array() ) {
+	public function __construct() {
+
+		call_user_func_array( array( $this, 'init' ), func_get_args() );
+
+	}
+
+	/**
+	 * Secondary constructor; Any supplied $args override class property defaults.
+	 *
+	 * @param string $object
+	 * @param string $id                    An specific ID of the setting. Can be a
+	 *                                      theme mod or option name.
+	 * @param array  $args                  Setting arguments.
+	 *
+	 * @return WP_Fields_API_Control $setting
+	 */
+	public function init( $object, $id, $args = array() ) {
+
+		global $wp_fields;
+
+		$this->object = $object;
+
 		$keys = array_keys( get_object_vars( $this ) );
+
 		foreach ( $keys as $key ) {
 			if ( isset( $args[ $key ] ) ) {
 				$this->$key = $args[ $key ];
 			}
 		}
 
-		$this->manager = $manager;
 		$this->id = $id;
-		if ( empty( $this->active_callback ) ) {
-			$this->active_callback = array( $this, 'active_callback' );
-		}
+
 		self::$instance_count += 1;
 		$this->instance_number = self::$instance_count;
 
@@ -156,232 +152,214 @@ class WP_Fields_API_Control {
 		}
 
 		$settings = array();
+
 		if ( is_array( $this->settings ) ) {
 			foreach ( $this->settings as $key => $setting ) {
-				$settings[ $key ] = $this->manager->get_setting( $setting );
+				$settings[ $key ] = $wp_fields->get_setting( $this->object, $setting );
 			}
 		} else {
-			$this->setting = $this->manager->get_setting( $this->settings );
+			$this->setting = $wp_fields->get_setting( $this->object, $this->settings );
 			$settings['default'] = $this->setting;
 		}
+
 		$this->settings = $settings;
+
 	}
 
 	/**
 	 * Enqueue control related scripts/styles.
 	 *
-	 * @since 3.4.0
 	 */
 	public function enqueue() {}
-
-	/**
-	 * Check whether control is active to current Customizer preview.
-	 *
-	 * @since 4.0.0
-	 * @access public
-	 *
-	 * @return bool Whether the control is active to the current preview.
-	 */
-	public final function active() {
-		$control = $this;
-		$active = call_user_func( $this->active_callback, $this );
-
-		/**
-		 * Filter response of WP_Customize_Control::active().
-		 *
-		 * @since 4.0.0
-		 *
-		 * @param bool                 $active  Whether the Customizer control is active.
-		 * @param WP_Customize_Control $control WP_Customize_Control instance.
-		 */
-		$active = apply_filters( 'customize_control_active', $active, $control );
-
-		return $active;
-	}
-
-	/**
-	 * Default callback used when invoking WP_Customize_Control::active().
-	 *
-	 * Subclasses can override this with their specific logic, or they may
-	 * provide an 'active_callback' argument to the constructor.
-	 *
-	 * @since 4.0.0
-	 * @access public
-	 *
-	 * @return bool Always true.
-	 */
-	public function active_callback() {
-		return true;
-	}
 
 	/**
 	 * Fetch a setting's value.
 	 * Grabs the main setting by default.
 	 *
-	 * @since 3.4.0
 	 *
 	 * @param string $setting_key
 	 * @return mixed The requested setting's value, if the setting exists.
 	 */
 	public final function value( $setting_key = 'default' ) {
+
 		if ( isset( $this->settings[ $setting_key ] ) ) {
 			return $this->settings[ $setting_key ]->value();
 		}
+
+		return null;
+
 	}
 
 	/**
 	 * Refresh the parameters passed to the JavaScript via JSON.
-	 *
-	 * @since 3.4.0
 	 */
 	public function to_json() {
+
 		$this->json['settings'] = array();
+
 		foreach ( $this->settings as $key => $setting ) {
 			$this->json['settings'][ $key ] = $setting->id;
 		}
 
 		$this->json['type'] = $this->type;
 		$this->json['priority'] = $this->priority;
-		$this->json['active'] = $this->active();
 		$this->json['section'] = $this->section;
 		$this->json['content'] = $this->get_content();
 		$this->json['label'] = $this->label;
 		$this->json['description'] = $this->description;
 		$this->json['instanceNumber'] = $this->instance_number;
+
 	}
 
 	/**
 	 * Get the data to export to the client via JSON.
 	 *
-	 * @since 4.1.0
 	 *
 	 * @return array Array of parameters passed to the JavaScript.
 	 */
 	public function json() {
+
 		$this->to_json();
+
 		return $this->json;
+
 	}
 
 	/**
 	 * Check if the theme supports the control and check user capabilities.
 	 *
-	 * @since 3.4.0
 	 *
 	 * @return bool False if theme doesn't support the control or user doesn't have the required permissions, otherwise true.
 	 */
 	public final function check_capabilities() {
+
+		global $wp_fields;
+
 		foreach ( $this->settings as $setting ) {
 			if ( ! $setting->check_capabilities() )
 				return false;
 		}
 
-		$section = $this->manager->get_section( $this->section );
-		if ( isset( $section ) && ! $section->check_capabilities() )
+		$section = $wp_fields->get_section( $this->object, $this->section );
+
+		if ( isset( $section ) && ! $section->check_capabilities() ) {
 			return false;
+		}
 
 		return true;
+
 	}
 
 	/**
-	 * Get the control's content for insertion into the Customizer pane.
+	 * Get the control's content for insertion.
 	 *
-	 * @since 4.1.0
 	 *
 	 * @return string Contents of the control.
 	 */
 	public final function get_content() {
+
 		ob_start();
+
 		$this->maybe_render();
+
 		$template = trim( ob_get_contents() );
+
 		ob_end_clean();
+
 		return $template;
+
 	}
 
 	/**
 	 * Check capabilities and render the control.
 	 *
-	 * @since 3.4.0
-	 * @uses WP_Customize_Control::render()
+	 * @uses WP_Fields_API_Control::render()
 	 */
 	public final function maybe_render() {
-		if ( ! $this->check_capabilities() )
+
+		if ( ! $this->check_capabilities() ) {
 			return;
+		}
 
 		/**
-		 * Fires just before the current Customizer control is rendered.
+		 * Fires just before the current control is rendered.
 		 *
-		 * @since 3.4.0
 		 *
-		 * @param WP_Customize_Control $this WP_Customize_Control instance.
+		 * @param WP_Fields_API_Control $this WP_Fields_API_Control instance.
 		 */
-		do_action( 'customize_render_control', $this );
+		do_action( 'fields_render_control_' . $this->object, $this );
 
 		/**
-		 * Fires just before a specific Customizer control is rendered.
+		 * Fires just before a specific control is rendered.
 		 *
 		 * The dynamic portion of the hook name, `$this->id`, refers to
 		 * the control ID.
 		 *
-		 * @since 3.4.0
 		 *
-		 * @param WP_Customize_Control $this {@see WP_Customize_Control} instance.
+		 * @param WP_Fields_API_Control $this {@see WP_Fields_API_Control} instance.
 		 */
-		do_action( 'customize_render_control_' . $this->id, $this );
+		do_action( 'fields_render_control_' . $this->object . '_' . $this->id, $this );
 
 		$this->render();
+
 	}
 
 	/**
 	 * Renders the control wrapper and calls $this->render_content() for the internals.
 	 *
-	 * @since 3.4.0
 	 */
 	protected function render() {
-		$id    = 'customize-control-' . str_replace( '[', '-', str_replace( ']', '', $this->id ) );
-		$class = 'customize-control customize-control-' . $this->type;
+
+		$id    = 'fields-control-' . str_replace( '[', '-', str_replace( ']', '', $this->id ) );
+		$class = 'fields-control fields-control-' . $this->type;
 
 		?><li id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $class ); ?>">
 			<?php $this->render_content(); ?>
 		</li><?php
+
 	}
 
 	/**
 	 * Get the data link attribute for a setting.
 	 *
-	 * @since 3.4.0
 	 *
 	 * @param string $setting_key
 	 * @return string Data link parameter, if $setting_key is a valid setting, empty string otherwise.
 	 */
 	public function get_link( $setting_key = 'default' ) {
-		if ( ! isset( $this->settings[ $setting_key ] ) )
-			return '';
 
-		return 'data-customize-setting-link="' . esc_attr( $this->settings[ $setting_key ]->id ) . '"';
+		if ( ! isset( $this->settings[ $setting_key ] ) ) {
+			return '';
+		}
+
+		return 'data-fields-setting-link="' . esc_attr( $this->settings[ $setting_key ]->id ) . '"';
+
 	}
 
 	/**
 	 * Render the data link attribute for the control's input element.
 	 *
-	 * @since 3.4.0
-	 * @uses WP_Customize_Control::get_link()
+	 * @uses WP_Fields_API_Control::get_link()
 	 *
 	 * @param string $setting_key
 	 */
 	public function link( $setting_key = 'default' ) {
+
 		echo $this->get_link( $setting_key );
+
 	}
 
 	/**
 	 * Render the custom attributes for the control's input element.
 	 *
-	 * @since 4.0.0
 	 * @access public
 	 */
 	public function input_attrs() {
+
 		foreach( $this->input_attrs as $attr => $value ) {
 			echo $attr . '="' . esc_attr( $value ) . '" ';
 		}
+
 	}
 
 	/**
@@ -392,11 +370,11 @@ class WP_Fields_API_Control {
 	 * Supports basic input types `text`, `checkbox`, `textarea`, `radio`, `select` and `dropdown-pages`.
 	 * Additional input types such as `email`, `url`, `number`, `hidden` and `date` are supported implicitly.
 	 *
-	 * Control content can alternately be rendered in JS. See {@see WP_Customize_Control::print_template()}.
+	 * Control content can alternately be rendered in JS. See {@see WP_Fields_API_Control::print_template()}.
 	 *
-	 * @since 3.4.0
 	 */
-	protected function render_content() {
+	public function render_content() {
+
 		switch( $this->type ) {
 			case 'checkbox':
 				?>
@@ -404,7 +382,7 @@ class WP_Fields_API_Control {
 					<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
 					<?php echo esc_html( $this->label ); ?>
 					<?php if ( ! empty( $this->description ) ) : ?>
-						<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<span class="description fields-control-description"><?php echo $this->description; ?></span>
 					<?php endif; ?>
 				</label>
 				<?php
@@ -413,13 +391,13 @@ class WP_Fields_API_Control {
 				if ( empty( $this->choices ) )
 					return;
 
-				$name = '_customize-radio-' . $this->id;
+				$name = '_fields-radio-' . $this->id;
 
 				if ( ! empty( $this->label ) ) : ?>
-					<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+					<span class="fields-control-title"><?php echo esc_html( $this->label ); ?></span>
 				<?php endif;
 				if ( ! empty( $this->description ) ) : ?>
-					<span class="description customize-control-description"><?php echo $this->description ; ?></span>
+					<span class="description fields-control-description"><?php echo $this->description ; ?></span>
 				<?php endif;
 
 				foreach ( $this->choices as $value => $label ) :
@@ -438,10 +416,10 @@ class WP_Fields_API_Control {
 				?>
 				<label>
 					<?php if ( ! empty( $this->label ) ) : ?>
-						<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+						<span class="fields-control-title"><?php echo esc_html( $this->label ); ?></span>
 					<?php endif;
 					if ( ! empty( $this->description ) ) : ?>
-						<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<span class="description fields-control-description"><?php echo $this->description; ?></span>
 					<?php endif; ?>
 
 					<select <?php $this->link(); ?>>
@@ -457,10 +435,10 @@ class WP_Fields_API_Control {
 				?>
 				<label>
 					<?php if ( ! empty( $this->label ) ) : ?>
-						<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+						<span class="fields-control-title"><?php echo esc_html( $this->label ); ?></span>
 					<?php endif;
 					if ( ! empty( $this->description ) ) : ?>
-						<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<span class="description fields-control-description"><?php echo $this->description; ?></span>
 					<?php endif; ?>
 					<textarea rows="5" <?php $this->link(); ?>><?php echo esc_textarea( $this->value() ); ?></textarea>
 				</label>
@@ -469,7 +447,7 @@ class WP_Fields_API_Control {
 			case 'dropdown-pages':
 				$dropdown = wp_dropdown_pages(
 					array(
-						'name'              => '_customize-dropdown-pages-' . $this->id,
+						'name'              => '_fields-dropdown-pages-' . $this->id,
 						'echo'              => 0,
 						'show_option_none'  => __( '&mdash; Select &mdash;' ),
 						'option_none_value' => '0',
@@ -481,7 +459,7 @@ class WP_Fields_API_Control {
 				$dropdown = str_replace( '<select', '<select ' . $this->get_link(), $dropdown );
 
 				printf(
-					'<label class="customize-control-select"><span class="customize-control-title">%s</span> %s</label>',
+					'<label class="fields-control-select"><span class="fields-control-title">%s</span> %s</label>',
 					$this->label,
 					$dropdown
 				);
@@ -490,47 +468,52 @@ class WP_Fields_API_Control {
 				?>
 				<label>
 					<?php if ( ! empty( $this->label ) ) : ?>
-						<span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+						<span class="fields-control-title"><?php echo esc_html( $this->label ); ?></span>
 					<?php endif;
 					if ( ! empty( $this->description ) ) : ?>
-						<span class="description customize-control-description"><?php echo $this->description; ?></span>
+						<span class="description fields-control-description"><?php echo $this->description; ?></span>
 					<?php endif; ?>
 					<input type="<?php echo esc_attr( $this->type ); ?>" <?php $this->input_attrs(); ?> value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); ?> />
 				</label>
 				<?php
 				break;
 		}
+
 	}
 
 	/**
 	 * Render the control's JS template.
 	 *
 	 * This function is only run for control types that have been registered with
-	 * {@see WP_Customize_Manager::register_control_type()}.
+	 * {@see WP_Fields_API::register_control_type()}.
 	 *
 	 * In the future, this will also print the template for the control's container
 	 * element and be override-able.
 	 *
-	 * @since 4.1.0
 	 */
-	final public function print_template() {
-	        ?>
-	        <script type="text/html" id="tmpl-customize-control-<?php echo $this->type; ?>-content">
-	                <?php $this->content_template(); ?>
-	        </script>
-	        <?php
+	public function print_template() {
+
+?>
+    <script type="text/html" id="tmpl-fields-<?php echo esc_attr( $this->object ); ?>-control-<?php echo esc_attr( $this->type ); ?>-content">
+        <?php $this->content_template(); ?>
+    </script>
+<?php
+
 	}
 
 	/**
 	 * An Underscore (JS) template for this control's content (but not its container).
 	 *
 	 * Class variables for this control class are available in the `data` JS object;
-	 * export custom variables by overriding {@see WP_Customize_Control::to_json()}.
+	 * export custom variables by overriding {@see WP_Fields_API_Control::to_json()}.
 	 *
-	 * @see WP_Customize_Control::print_template()
+	 * @see WP_Fields_API_Control::print_template()
 	 *
-	 * @since 4.1.0
 	 */
-	protected function content_template() {}
+	public function content_template() {
+
+		// Nothing by default
+
+	}
 
 }
