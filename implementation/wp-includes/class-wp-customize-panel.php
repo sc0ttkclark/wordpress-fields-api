2e39dbd7eefa5d143435653344adf18959bbaa89
Scott Kingsley Clark <lol@scottkclark.com>
2015-04-30 10:39:26 -0600

    Add Customizer Panel implementation and abstract Panel class to Field API

---
+++ implementation/wp-includes/class-wp-customize-panel.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * WordPress Customize Panel classes
+ *
+ * @package WordPress
+ * @subpackage Customize
+ * @since 4.0.0
+ */
+
+/**
+ * Customize Panel class.
+ *
+ * A UI container for sections, managed by the WP_Customize_Manager.
+ *
+ * @since 4.0.0
+ *
+ * @see WP_Customize_Manager
+ */
+class WP_Customize_Panel extends WP_Fields_API_Panel {
+
+	/**
+	 * WP_Customize_Manager instance.
+	 *
+	 * @access public
+	 * @var WP_Customize_Manager
+	 */
+	public $manager;
+
+	/**
+	 * Constructor.
+	 *
+	 * Any supplied $args override class property defaults.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
+	 * @param string               $id      An specific ID for the panel.
+	 * @param array                $args    Panel arguments.
+	 */
+	public function __construct( $manager, $id, $args = array() ) {
+
+		$this->manager = $manager;
+
+		parent::__construct( $this->type, $id, $args );
+
+		add_action( "fields_api_panel_active_{$this->object}", array( $this, 'customize_panel_active' ), 10, 2 );
+		add_action( "fields_api_render_panel_{$this->object}", array( $this, 'customize_render_panel' ) );
+		add_action( "fields_api_render_panel_{$this->object}_{$this->id}", array( $this, 'customize_render_panel_id' ) );
+
+	}
+
+	/**
+	 * Check whether panel is active to current Customizer preview.
+	 *
+	 * @access public
+	 *
+	 * @param bool                 $active  Whether the Fields API panel is active.
+	 * @param WP_Fields_API_Panel  $panel {@see WP_Fields_API_Panel} instance.
+	 *
+	 * @return bool Whether the panel is active to the current preview.
+	 */
+	final public function customize_section_active( $active, $panel ) {
+
+		/**
+		 * Filter response of {@see WP_Customize_Panel::active()}.
+		 *
+		 * @since 4.1.0
+		 *
+		 * @param bool                 $active  Whether the Customizer section is active.
+		 * @param WP_Customize_Section $section {@see WP_Customize_Panel} instance.
+		 */
+		$active = apply_filters( 'customize_panel_active', $active, $panel );
+
+		return $active;
+
+	}
+
+	/**
+	 * Backwards compatibility for fields_api_render_panel
+	 */
+	public function customize_render_panel() {
+
+		/**
+		 * Fires before rendering a Customizer panel.
+		 *
+		 * @since 4.0.0
+		 *
+		 * @param WP_Customize_Panel $this WP_Customize_Panel instance.
+		 */
+		do_action( 'customize_render_panel', $this );
+
+	}
+
+	/**
+	 * Backwards compatibility for fields_api_render_panel
+	 */
+	public function customize_render_panel_id() {
+
+		/**
+		 * Fires before rendering a specific Customizer panel.
+		 *
+		 * The dynamic portion of the hook name, `$this->id`, refers to
+		 * the ID of the specific Customizer panel to be rendered.
+		 *
+		 * @since 4.0.0
+		 */
+		do_action( "customize_render_panel_{$this->id}" );
+
+	}
+
+	/**
+	 * Checks required user capabilities and whether the theme has the
+	 * feature support required by the panel.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return bool False if theme doesn't support the panel or the user doesn't have the capability.
+	 */
+	final public function check_capabilities() {
+
+		if ( ! parent::check_capabilities() ) {
+			return false;
+		}
+
+		if ( $this->theme_supports && ! call_user_func_array( 'current_theme_supports', (array) $this->theme_supports ) ) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * Render the sections that have been added to the panel.
+	 *
+	 * @since 4.1.0
+	 * @access protected
+	 */
+	protected function render_content() {
+		?>
+		<li class="panel-meta accordion-section control-section<?php if ( empty( $this->description ) ) { echo ' cannot-expand'; } ?>">
+			<div class="accordion-section-title" tabindex="0">
+				<span class="preview-notice"><?php
+					/* translators: %s is the site/panel title in the Customizer */
+					echo sprintf( __( 'You are customizing %s' ), '<strong class="panel-title">' . esc_html( $this->title ) . '</strong>' );
+				?></span>
+			</div>
+			<?php if ( ! empty( $this->description ) ) : ?>
+				<div class="accordion-section-content description">
+					<?php echo $this->description; ?>
+				</div>
+			<?php endif; ?>
+		</li>
+		<?php
+	}
+}
--- includes/class-wp-fields-api-panel.php
+++ includes/class-wp-fields-api-panel.php
@@ -1,11 +1,17 @@
 <?php
 /**
- * Fields API Panel Class
- *
- * A UI container for sections, managed by the WP_Fields_API.
+ * WordPress Fields API Panel classes
  *
  * @package WordPress
  * @subpackage Fields_API
+ */
+
+/**
+ * Fields API Panel class.
+ *
+ * A UI container for sections, managed by WP_Fields_API.
+ *
+ * @see WP_Fields_API
  */
 class WP_Fields_API_Panel {
 
@@ -26,14 +32,6 @@ class WP_Fields_API_Panel {
 	 * @var int
 	 */
 	public $instance_number;
-
-	/**
-	 * WP_Customize_Manager instance.
-	 *
-	 * @access public
-	 * @var WP_Customize_Manager
-	 */
-	public $manager;
 
 	/**
 	 * Unique identifier.
@@ -84,7 +82,7 @@ class WP_Fields_API_Panel {
 	public $description = '';
 
 	/**
-	 * Customizer sections for this panel.
+	 * Fields API sections for this panel.
 	 *
 	 * @access public
 	 * @var array
@@ -104,10 +102,10 @@ class WP_Fields_API_Panel {
 	 *
 	 * @access public
 	 *
-	 * @see WP_Customize_Section::active()
+	 * @see WP_Fields_API_Section::active()
 	 *
 	 * @var callable Callback is called with one argument, the instance of
-	 *               {@see WP_Customize_Section}, and returns bool to indicate
+	 *               {@see WP_Fields_API_Section}, and returns bool to indicate
 	 *               whether the section is active (such as it relates to the URL
 	 *               currently being previewed).
 	 */
@@ -116,32 +114,45 @@ class WP_Fields_API_Panel {
 	/**
 	 * Constructor.
 	 *
-	 * Any supplied $args override class property defaults.
-	 *
-	 *
-	 * @param WP_Customize_Manager $manager Customizer bootstrap instance.
-	 * @param string               $id      An specific ID for the panel.
-	 * @param array                $args    Panel arguments.
+	 * Parameters are not set to maintain PHP overloading compatibility (strict standards)
 	 */
-	public function __construct( $manager, $id, $args = array() ) {
+	public function __construct() {
+
+		call_user_func_array( array( $this, 'init' ), func_get_args() );
+
+	}
+
+	/**
+	 * Secondary constructor; Any supplied $args override class property defaults.
+	 *
+	 * @param string $object
+	 * @param string $id                    An specific ID of the setting. Can be a
+	 *                                      theme mod or option name.
+	 * @param array  $args                  Section arguments.
+	 */
+	public function init( $object, $id, $args = array() ) {
+
+		$this->object = $object;
+
 		$keys = array_keys( get_object_vars( $this ) );
+
 		foreach ( $keys as $key ) {
 			if ( isset( $args[ $key ] ) ) {
 				$this->$key = $args[ $key ];
 			}
 		}
 
-		$this->manager = $manager;
 		$this->id = $id;
-		if ( empty( $this->active_callback ) ) {
-			$this->active_callback = array( $this, 'active_callback' );
-		}
+
 		self::$instance_count += 1;
 		$this->instance_number = self::$instance_count;
 
+		if ( empty( $this->active_callback ) ) {
+			$this->active_callback = array( $this, 'active_callback' );
+		}
+
 		$this->sections = array(); // Users cannot customize the $sections array.
 
-		return $this;
 	}
 
 	/**
@@ -152,6 +163,7 @@ class WP_Fields_API_Panel {
 	 * @return bool Whether the panel is active to the current preview.
 	 */
 	final public function active() {
+
 		$panel = $this;
 		$active = call_user_func( $this->active_callback, $this );
 
@@ -162,9 +174,10 @@ class WP_Fields_API_Panel {
 		 * @param bool               $active  Whether the Customizer panel is active.
 		 * @param WP_Customize_Panel $panel   {@see WP_Customize_Panel} instance.
 		 */
-		$active = apply_filters( 'customize_panel_active', $active, $panel );
+		$active = apply_filters( 'fields_api_panel_active_' . $this->object, $active, $panel );
 
 		return $active;
+
 	}
 
 	/**
@@ -178,54 +191,61 @@ class WP_Fields_API_Panel {
 	 * @return bool Always true.
 	 */
 	public function active_callback() {
+
 		return true;
+
 	}
 
 	/**
 	 * Gather the parameters passed to client JavaScript via JSON.
 	 *
-	 *
 	 * @return array The array to be exported to the client as JSON.
 	 */
 	public function json() {
+
 		$array = wp_array_slice_assoc( (array) $this, array( 'title', 'description', 'priority', 'type' ) );
+
 		$array['content'] = $this->get_content();
 		$array['active'] = $this->active();
 		$array['instanceNumber'] = $this->instance_number;
+
 		return $array;
+
 	}
 
 	/**
 	 * Checks required user capabilities and whether the theme has the
 	 * feature support required by the panel.
 	 *
-	 *
-	 * @return bool False if theme doesn't support the panel or the user doesn't have the capability.
+	 * @return bool False if the user doesn't have the capability.
 	 */
-	final public function check_capabilities() {
+	public function check_capabilities() {
+
 		if ( $this->capability && ! call_user_func_array( 'current_user_can', (array) $this->capability ) ) {
 			return false;
 		}
 
-		if ( $this->theme_supports && ! call_user_func_array( 'current_theme_supports', (array) $this->theme_supports ) ) {
-			return false;
-		}
-
 		return true;
+
 	}
 
 	/**
 	 * Get the panel's content template for insertion into the Customizer pane.
 	 *
-	 *
 	 * @return string Content for the panel.
 	 */
 	final public function get_content() {
+
 		ob_start();
+
 		$this->maybe_render();
+
 		$template = trim( ob_get_contents() );
+
 		ob_end_clean();
+
 		return $template;
+
 	}
 
 	/**
@@ -233,6 +253,7 @@ class WP_Fields_API_Panel {
 	 *
 	 */
 	final public function maybe_render() {
+
 		if ( ! $this->check_capabilities() ) {
 			return;
 		}
@@ -240,21 +261,20 @@ class WP_Fields_API_Panel {
 		/**
 		 * Fires before rendering a Customizer panel.
 		 *
-		 *
 		 * @param WP_Customize_Panel $this WP_Customize_Panel instance.
 		 */
-		do_action( 'customize_render_panel', $this );
+		do_action( "fields_api_render_panel_{$this->object}", $this );
 
 		/**
 		 * Fires before rendering a specific Customizer panel.
 		 *
 		 * The dynamic portion of the hook name, `$this->id`, refers to
 		 * the ID of the specific Customizer panel to be rendered.
-		 *
 		 */
-		do_action( "customize_render_panel_{$this->id}" );
+		do_action( "fields_api_render_panel_{$this->object}_{$this->id}" );
 
 		$this->render();
+
 	}
 
 	/**
@@ -287,8 +307,8 @@ class WP_Fields_API_Panel {
 		<li class="panel-meta accordion-section control-section<?php if ( empty( $this->description ) ) { echo ' cannot-expand'; } ?>">
 			<div class="accordion-section-title" tabindex="0">
 				<span class="preview-notice"><?php
-					/* translators: %s is the site/panel title in the Customizer */
-					echo sprintf( __( 'You are customizing %s' ), '<strong class="panel-title">' . esc_html( $this->title ) . '</strong>' );
+					/* translators: %s is the site/panel title in the Fields API */
+					echo sprintf( __( 'You are editing %s' ), '<strong class="panel-title">' . esc_html( $this->title ) . '</strong>' );
 				?></span>
 			</div>
 			<?php if ( ! empty( $this->description ) ) : ?>
