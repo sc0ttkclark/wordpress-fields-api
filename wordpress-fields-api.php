0ca7ab95fc2d0bb2c185900051094baa9f00b942
Scott Kingsley Clark <lol@scottkclark.com>
2015-09-21 13:11:19 -0500

    Implement Customizer override (without having to copy files over)

--- wordpress-fields-api.php
+++ wordpress-fields-api.php
@@ -13,7 +13,7 @@ Requires WP: 4.1
 */
 
 /**
- * @package WordPress
+ * @package    WordPress
  * @subpackage Fields_API
  */
 
@@ -23,7 +23,7 @@ Requires WP: 4.1
 define( 'WP_FIELDS_API_DIR', plugin_dir_path( __FILE__ ) );
 
 /**
- * On `plugins_loaded`, create an instnace of the Fields API manager class.
+ * On `plugins_loaded`, create an instance of the Fields API manager class.
  */
 function _wp_fields_api_include() {
 
@@ -33,4 +33,24 @@ function _wp_fields_api_include() {
 	$GLOBALS['wp_fields'] = new WP_Fields_API;
 
 }
+
 add_action( 'plugins_loaded', '_wp_fields_api_include', 9 );
+
+/**
+ * Implement Fields API Customizer instead of WP Core Customizer.
+ */
+function _wp_fields_api_customize_include() {
+
+	if ( ! ( ( isset( $_REQUEST['wp_customize'] ) && 'on' == $_REQUEST['wp_customize'] ) || ( is_admin() && 'customize.php' == basename( $_SERVER['PHP_SELF'] ) ) ) ) {
+		return;
+	}
+
+	require_once( WP_FIELDS_API_DIR . 'implementation/wp-includes/class-wp-customize-manager.php' );
+
+	// Init Customize class
+	$GLOBALS['wp_customize'] = new WP_Customize_Manager;
+
+}
+
+remove_action( 'plugins_loaded', '_wp_customize_include' );
+add_action( 'plugins_loaded', '_wp_fields_api_customize_include', 9 );
