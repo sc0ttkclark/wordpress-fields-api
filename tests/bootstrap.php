62223f5d95c72794b2d42f688bc735ba95ca32ba
Scott Kingsley Clark <lol@scottkclark.com>
2015-10-16 00:46:10 -0500

    Support VVV tests directly

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,12 +11,15 @@ define( 'WP_TESTS_FORCE_KNOWN_BUGS', true );
 // 1. `WP_DEVELOP_DIR` environment variable
 // 2. Plugin installed inside of WordPress.org developer checkout
 // 3. Tests checked out to /tmp
+// 4. VVV tests
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
 } else if ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
 	$test_root = '../../../../tests/phpunit';
 } else if ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
 	$test_root = '/tmp/wordpress-tests-lib';
+} else if ( file_exists( '/srv/www/wordpress-develop/tests/phpunit/includes/bootstrap.php' ) ) {
+	$test_root = '/srv/www/wordpress-develop/tests/phpunit';
 }
 
 require $test_root . '/includes/functions.php';
