05c1aa71c1d1ea69171306e24c1682dc75e8aa59
Scott Kingsley Clark <lol@scottkclark.com>
2015-12-28 04:42:40 -0600

    Add checkbox_value for core purposes (makes use of 'true' / 'false') strings and different default handling for them

--- includes/fields-api-controls.php
+++ includes/fields-api-controls.php
@@ -35,19 +35,48 @@ class WP_Fields_API_Text_Control extends WP_Fields_API_Control {
 class WP_Fields_API_Checkbox_Control extends WP_Fields_API_Control {
 
 	/**
+	 * Checkbox Value override.
+	 *
+	 * @access public
+	 *
+	 * @see WP_Fields_API_Checkbox_Control::checkbox_value()
+	 *
+	 * @var string Checkbox value, allowing for separation of value, 'default' value, and checkbox value.
+	 */
+	public $checkbox_value;
+
+	/**
 	 * {@inheritdoc}
 	 */
 	public function render_content() {
 
+			$checkbox_value = $this->checkbox_value();
 		?>
 		<label>
-			<input type="checkbox" value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link(); checked( $this->value() ); ?> />
+			<input type="checkbox" value="<?php echo esc_attr( $checkbox_value ); ?>" <?php $this->link(); checked( $checkbox_value, $this->value() ); ?> />
 			<?php echo esc_html( $this->label ); ?>
 			<?php if ( ! empty( $this->description ) ) : ?>
 				<span class="description fields-control-description"><?php echo $this->description; ?></span>
 			<?php endif; ?>
 		</label>
 		<?php
+
+	}
+
+	/**
+	 * Fetch a checkbox value and allows overriding for separation from field value and 'default' value.
+	 *
+	 * @return string The requested checkbox value.
+	 */
+	final public function checkbox_value() {
+
+		$value = $this->value();
+
+		if ( isset( $this->checkbox_value ) ) {
+			$value = $this->checkbox_value;
+		}
+
+		return $value;
 
 	}
 
