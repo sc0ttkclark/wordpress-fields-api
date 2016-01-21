0dd22eb37c11cdb3712d7dbc0371190eae588b78
Scott Kingsley Clark <lol@scottkclark.com>
2016-01-21 17:30:32 -0600

    Control fixes

--- implementation/wp-includes/fields-api/fields-api-controls.php
+++ implementation/wp-includes/fields-api/fields-api-controls.php
@@ -1,5 +1,49 @@
 <?php
 /**
+ * Fields API Textarea Control class.
+ *
+ * @see WP_Fields_API_Control
+ */
+class WP_Fields_API_Textarea_Control extends WP_Fields_API_Control {
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function render_content() {
+
+		?>
+		<label>
+			<?php if ( ! empty( $this->label ) ) : ?>
+				<span class="fields-control-title"><?php echo esc_html( $this->label ); ?></span>
+			<?php endif;
+			if ( ! empty( $this->description ) ) : ?>
+				<span class="description fields-control-description"><?php echo $this->description; ?></span>
+			<?php endif; ?>
+			<textarea <?php $this->input_attrs(); ?> <?php $this->link(); ?>><?php echo esc_textarea( $this->value( $this->item_id ) ); ?></textarea>
+		</label>
+		<?php
+
+	}
+
+	/**
+	 * Fetch a checkbox value and allows overriding for separation from field value and 'default' value.
+	 *
+	 * @return string The requested checkbox value.
+	 */
+	final public function checkbox_value() {
+
+		$value = $this->value();
+
+		if ( isset( $this->checkbox_value ) ) {
+			$value = $this->checkbox_value;
+		}
+
+		return $value;
+
+	}
+
+}
+/**
  * Fields API Checkbox Control class.
  *
  * @see WP_Fields_API_Control
@@ -23,9 +67,16 @@ class WP_Fields_API_Checkbox_Control extends WP_Fields_API_Control {
 	public function render_content() {
 
 			$checkbox_value = $this->checkbox_value();
+			$value = $this->value();
+
+			$checked = false;
+
+			if ( '' !== $value && $checkbox_value == $value ) {
+				$checked = true;
+			}
 		?>
 		<label>
-			<input type="checkbox" value="<?php echo esc_attr( $checkbox_value ); ?>" <?php $this->link(); checked( $checkbox_value, $this->value() ); ?> />
+			<input type="checkbox" value="<?php echo esc_attr( $checkbox_value ); ?>" <?php $this->link(); checked( $checked ); ?> />
 			<?php echo esc_html( $this->label ); ?>
 			<?php if ( ! empty( $this->description ) ) : ?>
 				<span class="description fields-control-description"><?php echo $this->description; ?></span>
