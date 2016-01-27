531830836f84bcb573861d9f5518c0bfa16a90a1
Scott Kingsley Clark <lol@scottkclark.com>
2016-01-27 16:59:42 -0600

    New doc: Starter examples for User and Term screens

---
+++ docs/examples/user/_starter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Register Fields API configuration
+ *
+ * @param WP_Fields_API $wp_fields
+ */
+function example_my_user_starter( $wp_fields ) {
+
+	// Object type: User
+	$object_type = 'user';
+
+	// Object name: n/a
+	$object_name = null;
+
+	// Screen: User Edit Profile
+	$screen_id = 'user-edit';
+
+	/////////////////////////
+	// Section: My Section //
+	/////////////////////////
+
+	$section_id = ''; // @todo Fill in section ID
+
+	$wp_fields->add_section( $object_type, $section_id, $object_name, array(
+		'title'  => __( '', 'my-text-domain' ), // @todo Fill in section heading, update text domain
+		'screen' => $screen_id,
+	) );
+
+	// My Field
+	$field_id = '';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'text', // @todo Change control type if needed
+			'section'     => $section_id,
+			'label'       => __( '', 'my-text-domain' ), // @todo Fill in label, update text domain
+			'description' => __( '', 'my-text-domain' ), // @todo Fill in description, update text domain
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+
+}
+add_action( 'fields_register', 'example_my_user_starter' );
