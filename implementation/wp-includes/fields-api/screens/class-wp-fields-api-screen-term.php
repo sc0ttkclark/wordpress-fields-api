72ab4ddcc93a671a5ceb245d1a0fece013517e9c
Scott Kingsley Clark <lol@scottkclark.com>
2016-01-27 16:58:56 -0600

    Register primary term fields only once, between screens, register unique controls for each screen

--- implementation/wp-includes/fields-api/screens/class-wp-fields-api-screen-term.php
+++ implementation/wp-includes/fields-api/screens/class-wp-fields-api-screen-term.php
@@ -16,6 +16,9 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 	 */
 	public function register_fields( $wp_fields ) {
 
+		// Make sure primary term fields are registered
+		$this->register_term_fields( $wp_fields );
+
 		// @todo Saving: Figure out compatibility with wp_insert_term / wp_update_term usage in edit-tags.php
 		// @todo Saving: Hook into create_term on save, check $object_name
 		// @todo Saving: Hook into edit_term on save, check $object_name
@@ -32,66 +35,58 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 			'display_title' => false,
 		) );
 
-		$field_args = array(
+		$control_args = array(
 			// @todo Needs validation callback
 			// @todo Needs 'form-required' class added to control wrapper somehow
-			'control' => array(
-				'type'        => 'text',
-				// @todo Control name should be tag-name (only for Add New screen)
-				'id'          => $this->id . '-name',
-				'section'     => $this->id . '-main',
-				'label'       => __( 'Name' ),
-				'description' => __( 'The name is how it appears on your site.' ),
-			),
+			'type'        => 'text',
+			'section'     => $this->id . '-main',
+			'label'       => __( 'Name' ),
+			'description' => __( 'The name is how it appears on your site.' ),
+			'fields'      => 'name',
 		);
 
-		$wp_fields->add_field( $this->object_type, 'name', null, $field_args );
+		// @todo Control name should be tag-name (only for Add New screen)
+		$wp_fields->add_control( $this->object_type, $this->id . '-name', null, $control_args );
 
-		$field_args = array(
-			'control' => array(
-				'type'                  => 'text',
-				'id'                    => $this->id . '-slug',
-				'section'               => $this->id . '-main',
-				'label'                 => __( 'Slug' ),
-				'description'           => __( 'The "slug" is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.' ),
-				'capabilities_callback' => array( $this, 'capability_is_global_terms_disabled' ),
-			),
+		$control_args = array(
+			'type'                  => 'text',
+			'section'               => $this->id . '-main',
+			'label'                 => __( 'Slug' ),
+			'description'           => __( 'The "slug" is the URL-friendly version of the name. It is usually all lowercase and contains only letters, numbers, and hyphens.' ),
+			'capabilities_callback' => array( $this, 'capability_is_global_terms_disabled' ),
+			'fields'                => 'slug',
 		);
 
-		$wp_fields->add_field( $this->object_type, 'slug', null, $field_args );
+		$wp_fields->add_control( $this->object_type, $this->id . '-slug', null, $control_args );
 
-		$field_args = array(
-			'control' => array(
-				'type'                         => 'dropdown-terms',
-				'id'                           => $this->id . '-parent',
-				'section'                      => $this->id . '-main',
-				'label'                        => __( 'Parent' ),
-				// @todo This description is only shown for 'category' == $object_name
-				// @todo Generic description for taxonomies, new label for register_taxonomy?
-				'description'                  => __( 'Categories, unlike tags, can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.' ),
-				'capabilities_callback'        => array( $this, 'capability_is_taxonomy_hierarchical' ),
-				'exclude_tree_current_item_id' => true,
-				'placeholder_text'             => __( 'None' ),
-			),
+		$control_args = array(
+			'type'                         => 'dropdown-terms',
+			'section'                      => $this->id . '-main',
+			'label'                        => __( 'Parent' ),
+			// @todo This description is only shown for 'category' == $object_name
+			// @todo Generic description for taxonomies, new label for register_taxonomy?
+			'description'                  => __( 'Categories, unlike tags, can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.' ),
+			'capabilities_callback'        => array( $this, 'capability_is_taxonomy_hierarchical' ),
+			'exclude_tree_current_item_id' => true,
+			'placeholder_text'             => __( 'None' ),
+			'fields'                       => 'parent',
 		);
 
-		$wp_fields->add_field( $this->object_type, 'parent', null, $field_args );
+		$wp_fields->add_control( $this->object_type, $this->id . '-parent', null, $control_args );
 
-		$field_args = array(
-			'control' => array(
-				'type'        => 'textarea',
-				'id'          => $this->id . '-description',
-				'section'     => $this->id . '-main',
-				'label'       => __( 'Description' ),
-				'description' => __( 'The description is not prominent by default; however, some themes may show it.' ),
-				'input_attrs' => array(
-					'rows' => '5',
-					'cols' => '40',
-				),
+		$control_args = array(
+			'type'        => 'textarea',
+			'section'     => $this->id . '-main',
+			'label'       => __( 'Description' ),
+			'description' => __( 'The description is not prominent by default; however, some themes may show it.' ),
+			'input_attrs' => array(
+				'rows' => '5',
+				'cols' => '40',
 			),
+			'fields'      => 'description',
 		);
 
-		$wp_fields->add_field( $this->object_type, 'description', null, $field_args );
+		$wp_fields->add_control( $this->object_type, $this->id . '-description', null, $control_args );
 
 		// Add example fields
 		parent::register_fields( $wp_fields );
@@ -100,13 +95,13 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 
 		// @todo Maintain some sort of filter on the $args, see taxonomy_parent_dropdown_args, has to be done during render of control
 		$dropdown_args = array(
-				'hide_empty'       => 0,
-				'hide_if_empty'    => false,
-				'taxonomy'         => $taxonomy,
-				'name'             => 'parent',
-				'orderby'          => 'name',
-				'hierarchical'     => true,
-				'show_option_none' => __( 'None' ),
+			'hide_empty'       => 0,
+			'hide_if_empty'    => false,
+			'taxonomy'         => $taxonomy,
+			'name'             => 'parent',
+			'orderby'          => 'name',
+			'hierarchical'     => true,
+			'show_option_none' => __( 'None' ),
 		);
 
 		/**
@@ -115,20 +110,21 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 		 * @since 3.7.0
 		 * @since 4.2.0 Added `$context` parameter.
 		 *
-		 * @param array  $dropdown_args {
-		 *     An array of taxonomy parent drop-down arguments.
+		 * @param array   $dropdown_args    {
+		 *                                  An array of taxonomy parent drop-down arguments.
 		 *
-		 *     @type int|bool $hide_empty       Whether to hide terms not attached to any posts. Default 0|false.
-		 *     @type bool     $hide_if_empty    Whether to hide the drop-down if no terms exist. Default false.
-		 *     @type string   $taxonomy         The taxonomy slug.
-		 *     @type string   $name             Value of the name attribute to use for the drop-down select element.
+		 * @type int|bool $hide_empty       Whether to hide terms not attached to any posts. Default 0|false.
+		 * @type bool     $hide_if_empty    Whether to hide the drop-down if no terms exist. Default false.
+		 * @type string   $taxonomy         The taxonomy slug.
+		 * @type string   $name             Value of the name attribute to use for the drop-down select element.
 		 *                                      Default 'parent'.
-		 *     @type string   $orderby          The field to order by. Default 'name'.
-		 *     @type bool     $hierarchical     Whether the taxonomy is hierarchical. Default true.
-		 *     @type string   $show_option_none Label to display if there are no terms. Default 'None'.
+		 * @type string   $orderby          The field to order by. Default 'name'.
+		 * @type bool     $hierarchical     Whether the taxonomy is hierarchical. Default true.
+		 * @type string   $show_option_none Label to display if there are no terms. Default 'None'.
 		 * }
-		 * @param string $taxonomy The taxonomy slug.
-		 * @param string $context  Filter context. Accepts 'new' or 'edit'.
+		 *
+		 * @param string  $taxonomy         The taxonomy slug.
+		 * @param string  $context          Filter context. Accepts 'new' or 'edit'.
 		 */
 		$dropdown_args = apply_filters( 'taxonomy_parent_dropdown_args', $dropdown_args, $taxonomy, 'new' );
 
@@ -139,7 +135,7 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 			/**
 			 * Fires after the Edit Category form fields are displayed.
 			 *
-			 * @since 2.9.0
+			 * @since      2.9.0
 			 * @deprecated 3.0.0 Use {$taxonomy}_edit_form_fields instead.
 			 *
 			 * @param object $tag Current category term object.
@@ -149,7 +145,7 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 			/**
 			 * Fires after the Edit Link Category form fields are displayed.
 			 *
-			 * @since 2.9.0
+			 * @since      2.9.0
 			 * @deprecated 3.0.0 Use {$taxonomy}_edit_form_fields instead.
 			 *
 			 * @param object $tag Current link category term object.
@@ -159,7 +155,7 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 			/**
 			 * Fires after the Edit Tag form fields are displayed.
 			 *
-			 * @since 2.9.0
+			 * @since      2.9.0
 			 * @deprecated 3.0.0 Use {$taxonomy}_edit_form_fields instead.
 			 *
 			 * @param object $tag Current tag term object.
@@ -196,6 +192,28 @@ class WP_Fields_API_Screen_Term extends WP_Fields_API_Screen {
 
 		// Save additional fields
 		return parent::save_fields( $item_id, $object_name );
+
+	}
+
+	/**
+	 * Register term fields once for all term screens
+	 *
+	 * @param WP_Fields_API $wp_fields
+	 */
+	public function register_term_fields( $wp_fields ) {
+
+		static $registered;
+
+		if ( $registered ) {
+			return;
+		}
+
+		$registered = true;
+
+		$wp_fields->add_field( $this->object_type, 'name' );
+		$wp_fields->add_field( $this->object_type, 'slug' );
+		$wp_fields->add_field( $this->object_type, 'parent' );
+		$wp_fields->add_field( $this->object_type, 'description' );
 
 	}
 
