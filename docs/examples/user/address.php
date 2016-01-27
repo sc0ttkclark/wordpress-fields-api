c3ef7a5e4e25319a0024746fdd6b1501fed8b8a0
Scott Kingsley Clark <lol@scottkclark.com>
2016-01-27 16:59:59 -0600

    New doc: Example of Address section for User screen

---
+++ docs/examples/user/address.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Register Fields API configuration
+ *
+ * @param WP_Fields_API $wp_fields
+ */
+function example_my_user_address( $wp_fields ) {
+
+	// Object type: User
+	$object_type = 'user';
+
+	// Object name: n/a
+	$object_name = null;
+
+	// Screen: User Edit Profile
+	$screen_id = 'user-edit';
+
+	//////////////////////
+	// Section: Address //
+	//////////////////////
+
+	$section_id = 'address';
+
+	$wp_fields->add_section( $object_type, $section_id, $object_name, array(
+		'title'  => __( 'Address', 'my-text-domain' ), // @todo Update text domain
+		'screen' => $screen_id,
+	) );
+
+	// Address Line 1
+	$field_id = 'address_1';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'text',
+			'section'     => $section_id,
+			'label'       => __( 'Address 1', 'my-text-domain' ), // @todo Update text domain
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+	// Address Line 2
+	$field_id = 'address_2';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'text',
+			'section'     => $section_id,
+			'label'       => __( 'Address 2', 'my-text-domain' ), // @todo Update text domain
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+	// City
+	$field_id = 'address_city';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'text',
+			'section'     => $section_id,
+			'label'       => __( 'City', 'my-text-domain' ), // @todo Update text domain
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+	// State / Region
+	$field_id = 'address_state';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'text',
+			'section'     => $section_id,
+			'label'       => __( 'State / Region', 'my-text-domain' ), // @todo Update text domain
+		    // You could use 'select' type instead and then
+			// pass in all states in 'choices' option with array( 'TX' => 'Texas' )
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+	// Zip / Postal Code
+	$field_id = 'address_zip';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'text',
+			'section'     => $section_id,
+			'label'       => __( 'Zip / Postal Code', 'my-text-domain' ), // @todo Update text domain
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+	// Zip / Postal Code
+	$field_id = 'address_country';
+	$field_args = array(
+		'control' => array(
+			'type'        => 'select',
+			'section'     => $section_id,
+			'label'       => __( 'Country', 'my-text-domain' ), // @todo Update text domain
+		    'choices' => array(
+			    'US' => 'United States',
+			    'CA' => 'Canada',
+			    // Add more here as needed, or use 'text' type instead for freeform
+		    ),
+		),
+	);
+
+	$wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
+
+
+}
+add_action( 'fields_register', 'example_my_user_address' );
