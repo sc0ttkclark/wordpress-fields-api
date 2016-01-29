63cad7c66bbd98f559f9d88c5e7988576e865803
Aaron Brazell <aaron@technosailor.com>
2016-01-29 18:18:38 +0000

    Scaffolding for General Settings

---
+++ implementation/wp-includes/fields-api/forms/class-wp-fields-api-form-settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is an implementation for Fields API for the Settings forms in the WordPress Dashboard
+ *
+ * @package    WordPress
+ * @subpackage Fields_API
+ */
+
+/**
+ * Class WP_Fields_API_Form_Settings
+ */
+class WP_Fields_API_Form_Settings extends WP_Fields_API_Form {
+
+}
