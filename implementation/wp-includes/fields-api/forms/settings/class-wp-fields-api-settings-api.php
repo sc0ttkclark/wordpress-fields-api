783420621b6241ded7cfac744b60f109c2528536
Scott Kingsley Clark <lol@scottkclark.com>
2016-01-31 12:48:43 -0600

    Check capabilities when registering settings to settings api; Add render_control for settings api compatibility

--- implementation/wp-includes/fields-api/forms/settings/class-wp-fields-api-settings-api.php
+++ implementation/wp-includes/fields-api/forms/settings/class-wp-fields-api-settings-api.php
@@ -28,30 +28,50 @@ class WP_Fields_API_Settings_API {
 		$sections = $wp_fields->get_sections( 'settings' );
 
 		foreach ( $sections as $section ) {
+			if ( ! $section->check_capabilities() ) {
+				continue;
+			}
+
 			// Get Form
 			$form_id = $section->form;
 			$section_id = $section->id;
+			$section_title = $section->title;
 
 			if ( is_object( $form_id ) ) {
 				$form_id = $form_id->id;
+			}
+
+			if ( ! $section->display_title ) {
+				$section_title = '';
 			}
 
 			// Get Setting Controls
 			$controls = $wp_fields->get_controls( $section->object_type, null, $section_id );
 
 			if ( $controls ) {
-				add_settings_section(
-					$section_id,
-					$section->title,
-					array( $section, 'render_description' ),
-					$form_id
-				);
+				$added_section = false;
 
 				foreach ( $controls as $control ) {
 					$field_id = $control->fields;
 
 					if ( empty( $field_id ) ) {
 						continue;
+					}
+
+					if ( ! $control->check_capabilities() ) {
+						continue;
+					}
+
+					if ( ! $added_section ) {
+
+						add_settings_section(
+							$section_id,
+							$section_title,
+							array( $section, 'render_description' ),
+							$form_id
+						);
+
+						$added_section = true;
 					}
 
 					$field_id = current( $field_id );
@@ -64,13 +84,20 @@ class WP_Fields_API_Settings_API {
 						$field_id = $field_id->id;
 					}
 
+					$settings_args = array(
+						'fields_api' => true,
+						'label_for'  => $field_id,
+						'control'    => $control,
+					);
+
 					// Add Settings API field
 					add_settings_field(
-						$field_id, // Field ID
-						$control->label, // Label
-						array( $control, 'maybe_render' ), // Render Callback
-						$form_id, // Page ID
-						$section_id // Section ID
+						$field_id,
+						$control->label,
+						array( $this, 'render_control' ),
+						$form_id,
+						$section_id,
+						$settings_args
 					);
 
 					// Register Setting
@@ -82,6 +109,42 @@ class WP_Fields_API_Settings_API {
 				}
 			}
 
+		}
+
+	}
+
+	/**
+	 * Render control for Settings API
+	 *
+	 * @param array $settings_args Settings args
+	 */
+	public function render_control( $settings_args ) {
+
+		if ( empty( $settings_args['fields_api'] ) || empty( $settings_args['control'] ) ) {
+			return;
+		}
+
+		/**
+		 * @var $control WP_Fields_API_Control
+		 */
+		$control = $settings_args['control'];
+
+		if ( ! $control->check_capabilities() ) {
+			return;
+		}
+
+		$description = trim( $control->description );
+
+		// Avoid outputting them in render_content()
+		$control->label       = '';
+		$control->description = '';
+
+		$control->render_content();
+
+		if ( 0 < strlen( $description ) ) {
+			?>
+			<p class="description"><?php echo wp_kses_post( $description ); ?></p>
+			<?php
 		}
 
 	}
