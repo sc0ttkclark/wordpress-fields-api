e1f5370aa0ee086ba30d2e2b302b3f83a84fa53d
Scott Kingsley Clark <lol@scottkclark.com>
2016-02-20 09:22:08 -0600

    Add 'editor_settings' option for control
    Shout out to @paulwilde

--- implementation/wp-includes/fields-api/control-types/class-wp-fields-api-wysiwyg-control.php
+++ implementation/wp-includes/fields-api/control-types/class-wp-fields-api-wysiwyg-control.php
@@ -17,13 +17,27 @@ class WP_Fields_API_WYSIWYG_Control extends WP_Fields_API_Control {
 	public $type = 'wysiwyg';
 
 	/**
+	 * Editor settings for use with wp_editor() third parameter
+	 *
+	 * @access public
+	 * @var array
+	 */
+	public $editor_settings = array();
+
+	/**
 	 * {@inheritdoc}
 	 */
 	protected function render_content() {
-		
+
+		// Get editor settings
+		$settings = $this->editor_settings;
+
+		// Get input attributes for textarea name
 		$this->input_attrs = $this->get_input_attrs();
-		$settings = array( 'textarea_name' => $this->input_attrs['name'] );
-		
+
+		// Set textarea name
+		$settings['textarea_name'] = $this->input_attrs['name'];
+
 		// actually render the tinyMCE box
 		wp_editor( $this->value(), $this->input_attrs['id'], $settings );
 
@@ -34,8 +48,10 @@ class WP_Fields_API_WYSIWYG_Control extends WP_Fields_API_Control {
 	 */
 	public function content_template() {
 
-		$settings = array( 'textarea_name' => '{{ data.input_name }}' );
-		wp_editor( '{{{ data.value }}}', '{{ data.input_id }}', $settings );
+		// @todo This needs further work and testing, not sure if it works right now, custom control settings don't pass through either
+
+		//$settings = array( 'textarea_name' => '{{ data.input_name }}' );
+		//wp_editor( '{{{ data.value }}}', '{{ data.input_id }}', $settings );
 
 	}
 
