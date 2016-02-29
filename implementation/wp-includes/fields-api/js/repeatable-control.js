39bad7e89a7ae1a2e8a5b2bee5b3bb9c9d30c0b3
Scott Kingsley Clark <lol@scottkclark.com>
2016-02-29 13:14:59 -0600

    Clean up initial formatting of JS

--- implementation/wp-includes/fields-api/js/repeatable-control.js
+++ implementation/wp-includes/fields-api/js/repeatable-control.js
@@ -1,53 +1,60 @@
-(function() {
-  var RepeaterView, ControlModel;
+( function () {
 
-  ControlModel = Backbone.Model.extend({
-    defaults: function() {
-      return {
-        type: 'text',
-        value: '',
-        input_id: '',
-        input_name: '',
-        repeatable: true
-      };
-    }
-  });
+	var RepeaterView, ControlModel;
 
-  RepeaterView = Backbone.View.extend({
-    el: '.fields-control',
-    events: {
-      'click .fields-repeatable-control-add-new': 'addControl',
-      'click .fields-repeatable-control-remove': 'removeControl'
-    },
-    addControl: function(event) {
-      var newControl, ControlView, data = {
-        type: this.$el.data( 'fields-type' ),
-        value: '',
-        input_id: this.$el.attr( 'id' ),
-        input_name: this.$el.data( 'fields-input-name' ),
-        repeatable: true
-      };
-      event.preventDefault();
-      ControlView = Backbone.View.extend({
-        templateID: 'fields-control-' + data.type + '-content',
-        template: function(data) {
-          return wp.template(this.templateID)(data);
-        },
-        render: function() {
-          return this.template(this.model.toJSON());
-        }
-      });
-      newControl = new ControlView({
-        model: new ControlModel
-      });
-      newControl.model.set( data );
-      return jQuery( event.srcElement ).before('<div class="fields-repeatable-input">' + (newControl.render()) + '</div>');
-    },
-    removeControl: function(event) {
-      jQuery( event.srcElement ).closest('.fields-repeatable-input').remove();
-    }
-  });
+	ControlModel = Backbone.Model.extend( {
+		defaults : function () {
+			return {
+				type       : 'text',
+				value      : '',
+				input_id   : '',
+				input_name : '',
+				repeatable : true
+			};
+		}
+	} );
 
-  new RepeaterView;
+	RepeaterView = Backbone.View.extend( {
+		el : '.fields-control', events : {
+			'click .fields-repeatable-control-add-new' : 'addControl',
+			'click .fields-repeatable-control-remove'  : 'removeControl'
+		}, addControl                           : function ( event ) {
 
-}).call(this);
+			var newControl, ControlView, data = {
+				type       : this.$el.data( 'fields-type' ),
+				value      : '',
+				input_id   : this.$el.attr( 'id' ),
+				input_name : this.$el.data( 'fields-input-name' ),
+				repeatable : true
+			};
+
+			event.preventDefault();
+
+			ControlView = Backbone.View.extend( {
+				templateID : 'fields-control-' + data.type + '-content',
+				template   : function ( data ) {
+					return wp.template( this.templateID )( data );
+				},
+				render     : function () {
+					return this.template( this.model.toJSON() );
+				}
+			} );
+
+			newControl = new ControlView( {
+				model : new ControlModel
+			} );
+
+			newControl.model.set( data );
+
+			jQuery( event.srcElement ).before( '<div class="fields-repeatable-input">' + (newControl.render()) + '</div>' );
+
+		}, removeControl                        : function ( event ) {
+
+			jQuery( event.srcElement ).closest( '.fields-repeatable-input' ).remove();
+
+		}
+										 } );
+
+	new RepeaterView;
+
+} ).call( this );
