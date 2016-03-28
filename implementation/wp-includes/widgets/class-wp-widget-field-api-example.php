67256bc457475556b2308939aa8267c684ac2694
Nícholas André <nicholas@iotecnologia.com.br>
2016-03-28 00:24:05 -0300

    Refactoring Widget Example

--- implementation/wp-includes/widgets/class-wp-widget-field-api-example.php
+++ implementation/wp-includes/widgets/class-wp-widget-field-api-example.php
@@ -1,5 +1,5 @@
 <?php
-class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
+class WP_Widget_Field_API_Example extends WP_Widgets_Fields_API {
 
     /**
      * Sets up the widgets name etc
@@ -13,13 +13,13 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
     }
 
     public function fields( $wp_fields ) {
-        // Object type: User
+        // Object type: widget
         $object_type = 'widget';
 
-        // Object name: n/a
+        // Object name: the id base
         $object_name = $this->id_base;
 
-        // Form: User Edit Profile
+        // Form: the id_base
         $form_id = $this->id_base;
 
         //////////////////////
@@ -28,7 +28,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
         $section_id   = 'address';
 
         $section_args = array(
-            'title' => __( 'Address', 'my-text-domain' ), // @todo Update text domain
+            'title' => __( 'Address', 'my-text-domain' ),
             'form'  => $form_id,
         );
 
@@ -40,7 +40,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
             'control' => array(
                 'type'    => 'text',
                 'section' => $section_id,
-                'label'   => __( 'Address 1', 'my-text-domain' ), // @todo Update text domain
+                'label'   => __( 'Address 1', 'my-text-domain' ),
             ),
         );
 
@@ -52,7 +52,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
             'control' => array(
                 'type'    => 'text',
                 'section' => $section_id,
-                'label'   => __( 'Address 2', 'my-text-domain' ), // @todo Update text domain
+                'label'   => __( 'Address 2', 'my-text-domain' ),
             ),
         );
 
@@ -64,7 +64,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
             'control' => array(
                 'type'    => 'text',
                 'section' => $section_id,
-                'label'   => __( 'City', 'my-text-domain' ), // @todo Update text domain
+                'label'   => __( 'City', 'my-text-domain' ),
             ),
         );
 
@@ -76,7 +76,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
             'control' => array(
                 'type'    => 'text',
                 'section' => $section_id,
-                'label'   => __( 'State / Region', 'my-text-domain' ), // @todo Update text domain
+                'label'   => __( 'State / Region', 'my-text-domain' ),
                 // You could use 'select' type instead and then
                 // pass in all states in 'choices' option with array( 'TX' => 'Texas' )
             ),
@@ -90,7 +90,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
             'control' => array(
                 'type'    => 'text',
                 'section' => $section_id,
-                'label'   => __( 'Zip / Postal Code', 'my-text-domain' ), // @todo Update text domain
+                'label'   => __( 'Zip / Postal Code', 'my-text-domain' ),
             ),
         );
 
@@ -102,7 +102,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
             'control' => array(
                 'type'    => 'select',
                 'section' => $section_id,
-                'label'   => __( 'Country', 'my-text-domain' ), // @todo Update text domain
+                'label'   => __( 'Country', 'my-text-domain' ),
                 'choices' => array(
                     'US' => 'United States',
                     'CA' => 'Canada',
@@ -113,6 +113,7 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
 
         $wp_fields->add_field( $object_type, $field_id, $object_name, $field_args );
     }
+
     /**
      * Outputs the content of the widget
      *
@@ -124,6 +125,8 @@ class WP_Widget_Field_Api_Example extends WP_Widgets_Fields_API {
     }
 }
 
-add_action( 'widgets_init', function() {
-    register_widget( 'WP_Widget_Field_Api_Example' );
-} );
+add_action( 'widgets_init', 'fields_api_register_widgets' );
+
+function fields_api_register_widgets() {
+    register_widget( 'WP_Widget_Field_API_Example' );
+}
